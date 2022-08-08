add66442377216580fd583144db018842fb0afff
sofiacmachado <sofia_cmachado@hotmail.com>
2022-08-08 13:36:36 +0100

    isDayBlocked Start and End

--- app/javascript/src/property/bookingWidget.jsx
+++ app/javascript/src/property/bookingWidget.jsx
@@ -7,37 +7,51 @@ import { safeCredentials, handleErrors } from '@utils/fetchHelper';
 import 'react-dates/lib/css/_datepicker.css';
 
 class BookingWidget extends React.Component {
-    state = {
-      authenticated: false,
-      startDate: null,
-      endDate: null,
-      focusedInput: null,
-      loading: false,
-      error: false,
-    }
-  
-    componentDidMount() {
-      fetch('/api/authenticated')
-        .then(handleErrors)
-        .then(data => {
-          this.setState({
-            authenticated: data.authenticated,
-          })
+  state = {
+    authenticated: false,
+    existingBookings: [],
+    startDate: null,
+    endDate: null,
+    focusedInput: null,
+    loading: false,
+    error: false,
+  }
+
+  componentDidMount() {
+    fetch('/api/authenticated')
+      .then(handleErrors)
+      .then(data => {
+        this.setState({
+          authenticated: data.authenticated,
         })
-    }
+      })
+
+    this.getPropertyBookings();
+  }
+
+  getPropertyBookings = () => {
+    fetch(`/api/properties/${this.props.property_id}/bookings`)
+      .then(handleErrors)
+      .then(data => {
+        console.log(data);
+        this.setState({
+          existingBookings: data.bookings,
+        })
+      })
+  }
   
     submitBooking = (e) => {
       if (e) { e.preventDefault(); }
       const { startDate, endDate } = this.state;
-      console.log(startDate, endDate);
+      console.log(startDate.format('MMM DD YYYY'), endDate.format('MMM DD YYYY'));
   
       fetch(`/api/bookings`, safeCredentials({
         method: 'POST',
           body: JSON.stringify({
             booking: {
               property_id: this.props.property_id,
-              start_date: null,
-              end_date: null,
+              start_date: startDate.format('MMM DD YYYY'),
+              end_date: endDate.format('MMM DD YYYY'),
             }
           })
       }))
@@ -51,43 +65,61 @@ class BookingWidget extends React.Component {
     }
   
     onDatesChange = ({ startDate, endDate }) => this.setState({ startDate, endDate })
-  
-    onFocusChange = (focusedInput) => this.setState({ focusedInput })
-  
-    render () {
-      const { authenticated, startDate, endDate, focusedInput } = this.state;
-      if (!authenticated) {
-        return (
-          <div className="border p-4 mb-4">
-            Please <a href={`/login?redirect_url=${window.location.pathname}`}>log in</a> to make a booking.
-          </div>
-        );
-      };
-  
-      const { price_per_night } = this.props;
-  
-      return (
-        <div className="border p-4 mb-4">
-          <form onSubmit={this.submitBooking}>
-            <h5>${price_per_night} <small>per night</small></h5>
-            <hr/>
-            <div className="mb-5">
-              <DateRangePicker
-                startDate={startDate} // momentPropTypes.momentObj or null,
-                startDateId="start_date" // PropTypes.string.isRequired,
-                endDate={endDate} // momentPropTypes.momentObj or null,
-                endDateId="end_date" // PropTypes.string.isRequired,
-                onDatesChange={this.onDatesChange}
-                focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
-                numberOfMonths={1}
-              />
-            </div>
-            <button type="submit" className="btn btn-large btn-danger btn-block">Book</button>
-          </form>
+
+onFocusChange = (focusedInput) => this.setState({ focusedInput })
+
+isDayBlockedStart = day => this.state.existingBookings.filter(b => day.isBetween(b.start_date, b.end_date, 'day', '])')).length > 0
+isDayBlockedEnd = day => this.state.existingBookings.filter(b => day.isBetween(b.start_date, b.end_date, 'day', '[)')).length > 0
+
+render () {
+  const { authenticated, startDate, endDate, focusedInput } = this.state;
+  if (!authenticated) {
+    return (
+      <div className="border p-4 mb-4">
+        Please <a href={`/login?redirect_url=${window.location.pathname}`}>log in</a> to make a booking.
+      </div>
+    );
+  };
+
+  const { price_per_night } = this.props;
+
+  let days;
+  if (startDate && endDate) {
+    days = endDate.diff(startDate, 'days');
+  }
+
+  return (
+    <div className="border p-4 mb-4">
+      <form onSubmit={this.submitBooking}>
+        <h5>${price_per_night} <small>per night</small></h5>
+        <hr/>
+        <div className="mb-5">
+          <DateRangePicker
+            startDate={startDate} // momentPropTypes.momentObj or null,
+            startDateId="start_date" // PropTypes.string.isRequired,
+            endDate={endDate} // momentPropTypes.momentObj or null,
+            endDateId="end_date" // PropTypes.string.isRequired,
+            onDatesChange={this.onDatesChange}
+            focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
+            isDayBlocked={this.state.focusedInput === 'startDate'
+            ? this.isDayBlockedStart
+            : isDayBlockedEnd
+            } // block already booked dates
+            numberOfMonths={1}
+          />
         </div>
-      )
-    }
+        {days && (
+          <div className="d-flex justify-content-between">
+            <p>Total</p>
+            <p>${(price_per_night * days).toLocaleString()}</p>
+          </div>
+        )}
+        <button type="submit" className="btn btn-large btn-danger btn-block">Book</button>
+      </form>
+    </div>
+  )
+}
   }
   
   export default BookingWidget;
