0132833f685eb0263ffd0a082b4b90cf1244a8c7
sofiacmachado <sofia_cmachado@hotmail.com>
2022-08-08 11:41:41 +0100

    Log In Done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,9 @@ class StaticPagesController < ApplicationController
     @data = { property_id: params[:id] }.to_json
     render 'property'
   end
+
+  def login
+    render 'login'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   get '/property/:id' => 'static_pages#property'
+  get '/login' => 'static_pages#login'
 
   namespace :api do
     # Add routes below this line
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
     resources :properties, only: [:index, :show]
     resources :bookings, only: [:create]
 
+    get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
 
   end
